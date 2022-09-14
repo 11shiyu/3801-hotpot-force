714c9bb77b1d492edcbe29fc0a816c4ec47b5982
leoUchiha <95334690+leoUchiha@users.noreply.github.com>
2022-09-14 13:09:47 +0800

    Add files via upload
    initial

---
+++ HotPotForce/TastyGo/src/main/java/com/hotpotforce/TastyGoApplication.java
@@ -0,0 +1,13 @@
+package com.hotpotforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastyGoApplication.class, args);
+    }
+
+}
---
+++ HotPotForce/TastyGo/src/main/java/com/hotpotforce/pojo/Users.java
@@ -0,0 +1,11 @@
+package com.hotpotforce.pojo;
+
+
+public class Users {
+    private int ID;
+    private String username;
+    private String password;
+    private String Email;
+
+
+}
