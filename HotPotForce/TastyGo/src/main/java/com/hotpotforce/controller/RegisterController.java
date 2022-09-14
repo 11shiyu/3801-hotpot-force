1684c55193eed21f96a6d1c9c37bcd2ecab6e8a0
PendaZz <littlexiongmao@gmail.com>
2022-09-14 15:47:15 +1000

    Fix

--- HotPotForce/TastyGo/src/main/java/com/hotpotforce/controller/RegisterController.java
+++ HotPotForce/TastyGo/src/main/java/com/hotpotforce/controller/RegisterController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/index")
+
 public class RegisterController {
     @GetMapping("/registeration")
     public String register() {
