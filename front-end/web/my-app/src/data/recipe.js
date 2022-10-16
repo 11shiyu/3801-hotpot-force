2fe972b889a2afa021fd851800d23ba508615e25
PendaZz <littlexiongmao@gmail.com>
2022-10-16 13:18:00 +1000

    sample recipe

---
+++ front-end/web/my-app/src/data/recipe.js
@@ -0,0 +1,38 @@
+export const recipeList = [
+    {
+        cookingTime: 17,
+        recipeName: "Adobo",
+        description: "A way of cooking introduced by the Spanish and Portuguese led to adobo, a traditional Philippine dish. Chicken or pork are marinated and cooked in white vinegar, soy sauce, garlic, peppercorns and bay leaves, then poured over rice for a flavorful combination.",
+        nationality: "Philippines",
+        photoPath: "https://www.pexels.com/zh-cn/photo/6525933/",
+        cultureBackground: "Like many cultures based in warm climates, Filipino natives developed various methods of preserving food. Adobo utilizes the acid in the vinegar and the high salt content of soy sauce to produce an undesirable environment for bacteria. Its delicious flavor and preserving qualities served to increase adobo's popularity. The adobo was traditionally cooked in clay pots but today is made in more common metal pots or woks.\n\nWhen the Spanish invaded and settled in the Philippines during the 16th century, they witnessed this traditional Filipino cooking method and called it adobo, which is the Spanish word for marinade.",
+        id: 1
+    },
+    {
+        cookingTime: 18,
+        recipeName: "Sushi",
+        description: "Using vinegared rice wrapped in thin sheets of seaweed, cucumber, shiso or soy paper leaves, sushi isn't always a fish dish; it's anything rolled with the rice, from veggies to meats to, yes, raw fish (sashimi).",
+        nationality: "Japan",
+        photoPath: "https://www.pexels.com/zh-cn/photo/357756/",
+        cultureBackground: "The concept of sushi was likely introduced to Japan in the ninth century, and became popular there as Buddhism spread.\nIn the 1820s, a man named Hanaya Yohei found himself in Edo. Yohei is often considered the creator of modern nigiri sushi, or at the very least its first great marketer. In 1824, Yohei opened the first sushi stall in the Ryogoku district of Edo. Ryogoku translates to the place between two countries because of its location along the banks of the Sumida River. Yohei chose his location wisely, setting up his stall near one of the few bridges that crossed the Sumida. He took advantage of a more modern speed fermentation process, adding rice vinegar and salt to freshly cooked rice and letting it sit for a few minutes. He then served the sushi in a hand-pressed fashion, topping a small ball of rice with a thin slice of raw fish, fresh from the bay. Because the fish was so fresh, there was no need to ferment or preserve it. Sushi could be made in a matter of minutes, rather than in hours or days.",
+        id: 2
+    },
+    {
+        cookingTime: 19,
+        recipeName: "Rice Paper Rolls",
+        description: "1.	Peanut Sauce: Combine the Peanut Dipping Sauce ingredients. Mix briefly (it won’t come together), then microwave for 30 seconds. Mix again until smooth. Set aside to cool. Adjust sour with vinegar, salt with salt and spiciness to taste.  Thickness can be adjusted with milk or water once cooled.\n2.	Place vermicelli noodles in a bowl and cover with warm water for 2 minutes, then drain (or follow packet instructions).\n3.	Peel the prawns, slice in half lengthwise and devein (watch video).\n4.	Remove the crunchy core of the lettuce leaves (watch video).\n5.	Tip – LETTUCE BUNDLE (Note 4): Place some vermicelli noodles and bean sprouts in a lettuce leaf, then roll it up, finishing seam side down. Repeat.\n6.	Fill a large bowl with warm water. The bowl doesn’t need to be large enough to fit the whole rice paper in one go.\n7.	Place two rice papers together (if using 2). Note which side is the smooth side – this is supposed to be the outside of the spring roll. Submerge the rice papers into the water (both of them at the same time, together) for 2 seconds. If your bowl isn’t large enough to fit the whole rice paper in one go, that’s fine, just rotate it and count 2 seconds for each section you submerge into the water.\n8.	Place both the rice papers (one on top of the other, they will stick together) on a board or the counter with the smooth side down.\n9.	On the top part of the rice paper, place 3 prawns with a mint leaf in between, as per the photo below.\n10.	Place the lettuce bundle with the seam side down onto the middle of the rice paper.\n11.	Fold the left and right edges of the rice paper in, then starting from the bottom, roll up to cover the lettuce bundle. Then keep rolling firmly. The rice paper is sticky, it will seal itself.\n12.	If you placed the ingredients on the rice paper as per the photo below, your rice paper rolls should look pretty with the prawn and mint leaves on the smooth side of the roll and the seam on the side or underside of the roll.\n13.	Serve immediately with the peanut dipping sauce.",
+        nationality: "Vietnamese",
+        photoPath: "https://unsplash.com/photos/mFhGgUWNWVg",
+        cultureBackground: "culture Background",
+        id: 3
+    },
+    {
+        cookingTime: 20,
+        recipeName: "Pizza Margherita",
+        description: "Step 1. \nPlace a pizza stone or tiles on the middle rack of your oven and turn heat to its highest setting. Let it heat for at least an hour.\nStep 2. \nPut the sauce in the center of the stretched dough and use the back of a spoon to spread it evenly across the surface, stopping approximately ½ inch from the edges.\nStep 3. \nDrizzle a little olive oil over the pie. Break the cheese into large pieces and place these gently on the sauce. Scatter basil leaves over the top.\nStep 4. \nUsing a pizza peel, pick up the pie and slide it onto the heated stone or tiles in the oven. Bake until the crust is golden brown and the cheese is bubbling, approximately 4 to 8 minutes.",
+        nationality: "Margherita",
+        photoPath: "https://unsplash.com/photos/jvWZYnxBDlQ",
+        cultureBackground: "In June 1889 the pizzaiolo Raffaele Esposito, Pizzeria Brandi's chef, invented a dish called 'Pizza Margherita' in honor of the Queen of Italy, Margherita of Savoy, and the Italian unification, since toppings are tomato (red), mozzarella (white) and basil (green), ingredients inspired by the colors of the national flag of Italy.\nThe origins of Pizza Margherita came from mixing similar toppings that were was already present in Naples between 1796 and 1810. In 1849 Emanuele Rocco recorded different pizza toppings like basil, tomatoes and thin slices of mozzarella; the mozzarella was thinly sliced, and added to the toppings already present.",
+        id: 4
+    }
+]
